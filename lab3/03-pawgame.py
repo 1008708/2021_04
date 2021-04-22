b5f73a3d220859581639fdb425a81e324a803f43
Igor <1008708@gmail.com>
2021-04-22 09:08:15 +0300

    В pawgame рисуем картинку 3 раза и обработка события нажатия клавиатуры для масштаба

--- lab3/03-pawgame.py
+++ lab3/03-pawgame.py
@@ -7,33 +7,43 @@ from pygame.draw import *
 # После импорта библиотеки, необходимо её инициализировать:
 pygame.init()
 
-def eye(x, y, color, size):
+def eye(screen, color, x, y, size):
+    '''
+    x, y - центр глаз
+    цвет, радиус
+    '''
     circle(screen, color, (x, y), size, 0)
     circle(screen, "black", (x, y), size // 3, 0)
 
+def mouth(screen, color, x, y, width, height):
+    '''
+    x, y - левый верх рта
+    цвет
+    '''
+    rect(screen, color, (x, y, width, height), 0)
+    rect(screen, 'WHITE', (x, y + height // 2, width, height // 2), 1)
+    
 
-def elad(cenx, ceny, size_head):
+def elad(screen, cenx, ceny, size_head):
     # здесь будут рисоваться фигуры
     # ...
     color = (200, 150, 150)
     circle(screen, color, (cenx, ceny), size_head, 0)
     
-    x_eye1 = cenx - size_head // 2; y_eye1 = ceny - size_head  // 2
-    x_eye2 = cenx + size_head // 2; y_eye2 = ceny - size_head  // 2
-    
+    y_eye = ceny - size_head  // 2
     color = (30, 150, 150)
-    eye(x_eye1, y_eye1, color, size_head // 10)
-    eye(x_eye2, y_eye2, color, size_head // 10)
-        
+    for x_eye1 in (cenx - size_head // 2, cenx + size_head // 2):
+        eye(screen, color, x_eye1, y_eye, size_head // 10)
+            
     color = (230, 100, 150)
     x_mouth = cenx - size_head // 2
     y_mouth = ceny + size_head // 2.5
-    rect(screen, color, (x_mouth, y_mouth, size_head, size_head // 10))
-
-    color = (230, 200, 250)
+    mouth(screen, color, x_mouth, y_mouth, size_head, size_head // 10)
+    
+    color = (20, 200, 250)
     nouse = [(cenx, ceny - size_head // 10), (cenx - size_head // 10, ceny), 
              (cenx + size_head // 10, ceny), (cenx, ceny - size_head // 10)]
-    polygon(screen, (0, 0, 255), nouse, 0)
+    polygon(screen, color, nouse, 0)
     
 # И создать окно:
 screen = pygame.display.set_mode((800, 800))
@@ -43,25 +53,20 @@ finished = False
 FPS = 50
 scale = 150
 
-screen.fill('WHITE')
-elad(200, 200, scale)
-pygame.display.update()
-
 while not finished:
+    clock.tick(FPS) 
     for event in pygame.event.get():
-        clock.tick(FPS) 
         key = pygame.key.get_pressed()
         if event.type == pygame.QUIT:
             finished = True
         if key[pygame.K_LEFT]:
             scale -= 5
-            screen.fill('WHITE')
-            elad(200, 200, scale)
-            pygame.display.flip()
-        if key[pygame.K_RIGHT]:
+        elif key[pygame.K_RIGHT]:
             scale += 5
-            screen.fill('WHITE')
-            elad(200, 200, scale)
-            pygame.display.flip()
+    screen.fill('WHITE')
+    elad(screen, 400, 200, scale)
+    elad(screen, 100, 200, scale // 2)
+    elad(screen, 700, 200, scale // 2)
+    pygame.display.update()
             
 pygame.quit()
